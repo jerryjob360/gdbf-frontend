ef635dfc7c96581c3157a49806f2b44f7bbe8b9b
Jerry Job <jerryjob360@gmail.com>
2026-07-16 16:48:57 +0300

    Changes in the homepage and the services paeg to remove unwanted content

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -2,7 +2,7 @@
 // import forest_day from "../assets/forest_day.jpg";
 // import reforestation from "../assets/reforestation.jpg";
 // import reforestation_campaign from "../assets/reforestation_campaign.jpg";
-import seminar_member from "../assets/seminar_member.jpg";
+import organizational_effectiveness from "../assets/organizational_effectiveness_1.jpeg";
 import env_health from "../assets/environmental_health_1.jpeg";
 import resilience from "../assets/resilience_1.jpeg";
 import indigineous from "../assets/indigenous_1.jpeg";
@@ -26,7 +26,7 @@ export const MenuList = [
   },
   {
     name: "",
-    image: seminar_member,
+    image: organizational_effectiveness,
     description: 'We enhance organizational effectiveness and sustainability through good governance, strategic partnerships, capacity development, and resource mobilization.',
   },
   // {
