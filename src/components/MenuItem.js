2a6f20ce4e996785f9edf268d3b45561d77a2eca
Jerry Job <jerryjob360@gmail.com>
2026-07-16 17:21:07 +0300

    Setting absolute image sizes in the services page

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,12 +1,11 @@
 import React from "react";
-import '../styles/Menu.css';
 
 function MenuItem({ image, name, description }) {
   return (
     <div className="menuItem">
-      <img src={image} alt={name} className="menuItemImage" />
-      <h1>{name}</h1>
-      <p>{description}</p>
+      <div style={{ backgroundImage: `url(${image})` }}> </div>
+      <h1> {name} </h1>
+      <p> {description} </p>
     </div>
   );
 }
