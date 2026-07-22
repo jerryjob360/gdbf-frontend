717e044afb82d8131c7a4d60b5d9c70704f03a18
Jerry Job <jerryjob360@gmail.com>
2026-07-22 10:39:38 +0300

    Retaining google translate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -87,7 +87,7 @@ function Navbar() {
                     </div>
                 </div>
                 <div className='rightSide'>
-                    {/* <div id="google_translate_element" className="google-translate-container"></div> */}
+                    <div id="google_translate_element" className="google-translate-container"></div>
                     <NavLink
                         to='/'
                         className={({ isActive }) => 
