c984bf125ee3bae821eb55f3bf492b15a03a0a90
Jerry Job <jerryjob360@gmail.com>
2026-07-22 10:28:40 +0300

    Services text padding

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import '../styles/About.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { useStateContext } from '../contexts/contextProvider';
-import BannerImage from '../assets/back.jpeg'
+import BannerImage from '../assets/frontpg.png'
 
 function About() {
   
