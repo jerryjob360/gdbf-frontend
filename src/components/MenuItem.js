6e558a04bd446732e894f427f336f731b2ec3c31
Jerry Job <jerryjob360@gmail.com>
2026-07-22 10:04:36 +0300

    Services text padding

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -5,7 +5,7 @@ function MenuItem({ image, name, description }) {
   return (
     <div className="menuItem">
       <img src={image} alt={name} className="menuItemImage" />
-      <h1>{name}</h1>
+      {/* <h1>{name}</h1> */}
       <p>{description}</p>
     </div>
   );
