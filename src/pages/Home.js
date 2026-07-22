58b09b9e14be6dbdba640799a72492317e6cf019
Jerry Job <jerryjob360@gmail.com>
2026-07-22 09:28:27 +0300

    Setting the new fornt page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import Services from './Services';
-import BannerImage from '../assets/fontpg.png';
+import BannerImage from '../assets/frontpg.png';
 import '../styles/Home.css'
 import { useStateContext } from '../contexts/contextProvider';
 
